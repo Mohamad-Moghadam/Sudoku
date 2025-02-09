642d9bad325e51ecaf8b9d24f137816e35879834
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-09 16:00:41 +0330

    🐛 input bug fixed

--- menu.py
+++ menu.py
@@ -11,44 +11,71 @@ from random import randint
 
 
 def menu():
-    while True:
-        print("What is your name? ")
-        name = input()
+    print("What is your name? ")
+    name = input()
 
-        print(f"\nWelcome {name}. \n1. Start game \n2. Statistics \n3. Exit game\n ")
-        choice = int(input())
+    print(f"\nWelcome {name}. \n1. Start game \n2. Statistics \n3. Exit game\n ")
+    choice = int(input())
 
-        if choice == 1:
-            print(
-                f"\nwhich mode do you wanna play? \n1. Easy \n2. Medium \n3. Hard \n4. Exit\n"
-            )
-            game_mode = input()
-            if game_mode == 1:
-                game(1)
-            elif game_mode == 2:
-                game(2)
-            elif game_mode == 3:
-                game(3)
-            elif game_mode == 4:
-                return
-                # choice == 2:
-                # with open("C:\Users\USER\Desktop\kelassor\3rd week\Sudoku\statistics.json" , encoding='utf-8') as file:
-                data = json.load(file)
-                pprint(data)
-                return
-        elif choice == 3:
+    if choice == 1:
+        print(
+            f"\nwhich mode do you wanna play? \n1. Easy \n2. Medium \n3. Hard \n4. Exit\n"
+        )
+        game_mode = input()
+        if game_mode == 1:
+            game(1)
+        elif game_mode == 2:
+            game(2)
+        elif game_mode == 3:
+            game(3)
+        elif game_mode == 4:
             return
+            # choice == 2:
+            # with open("C:\Users\USER\Desktop\kelassor\3rd week\Sudoku\statistics.json" , encoding='utf-8') as file:
+            data = json.load(file)
+            pprint(data)
+            return
+    elif choice == 3:
+        return
 
 
 def game(difficulty):
 
     if difficulty == 1:
-        for _ in range(1):
-            n = randint(1, 8)
-            row1 = [[n, None, n + 1], [None, n + 2, None], [None, None, None]]
-            row2 = [[None, n + 2, None], [n, None, None], [n + 1, None, None]]
-            row3 = [[None, n, None, None], [n + 2, None, None], [None, None, n + 1]]
-        print(row1, row2)
+        a = randint(1, 10)
+        b = randint(1, 10) - [a]
+        c = randint(1, 10) - [a, b]
+        row1 = [
+            [None, None, a],
+            [None, c, None],
+            [None, None, b][None, b, None],
+            [a, None, None],
+            [None, None, c][c, None, None],
+            [b, None, None],
+            [None, a, None],
+        ]
+
+        row2 = [
+            [None, None, None],
+            [None, None, a],
+            [c, b, None][None, None, c],
+            [None, b, None],
+            [None, None, a][None, a, b],
+            [None, None, c],
+            [None, None, None],
+        ]
+
+        row3 = [
+            [None, c, None],
+            [None, None, b],
+            [a, None, None][a, None, None],
+            [c, None, None],
+            [b, None, None][b, None, None],
+            [None, a, None],
+            [None, c, None],
+        ]
+
+        print(row1, row2, row3)
 
 
 menu()
