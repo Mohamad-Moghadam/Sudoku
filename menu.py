6d85d2fa4ecc3147bb3c59e7bf28b097d4b9fc57
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-09 16:02:28 +0330

    🐛 list error fixed

--- menu.py
+++ menu.py
@@ -11,40 +11,41 @@ from random import randint
 
 
 def menu():
-    print("What is your name? ")
-    name = input()
+    while True:
+        print("What is your name? ")
+        name = input()
 
-    print(f"\nWelcome {name}. \n1. Start game \n2. Statistics \n3. Exit game\n ")
-    choice = int(input())
+        print(f"\nWelcome {name}. \n1. Start game \n2. Statistics \n3. Exit game\n ")
+        choice = int(input())
 
-    if choice == 1:
-        print(
-            f"\nwhich mode do you wanna play? \n1. Easy \n2. Medium \n3. Hard \n4. Exit\n"
-        )
-        game_mode = input()
-        if game_mode == 1:
-            game(1)
-        elif game_mode == 2:
-            game(2)
-        elif game_mode == 3:
-            game(3)
-        elif game_mode == 4:
+        if choice == 1:
+            print(
+                f"\nwhich mode do you wanna play? \n1. Easy \n2. Medium \n3. Hard \n4. Exit\n"
+            )
+            game_mode = int(input())
+            if game_mode == 1:
+                game(1)
+            elif game_mode == 2:
+                game(2)
+            elif game_mode == 3:
+                game(3)
+            elif game_mode == 4:
+                return
+                # choice == 2:
+                # with open("C:\Users\USER\Desktop\kelassor\3rd week\Sudoku\statistics.json" , encoding='utf-8') as file:
+                data = json.load(file)
+                pprint(data)
+                return
+        elif choice == 3:
             return
-            # choice == 2:
-            # with open("C:\Users\USER\Desktop\kelassor\3rd week\Sudoku\statistics.json" , encoding='utf-8') as file:
-            data = json.load(file)
-            pprint(data)
-            return
-    elif choice == 3:
-        return
 
 
 def game(difficulty):
 
     if difficulty == 1:
         a = randint(1, 10)
-        b = randint(1, 10) - [a]
-        c = randint(1, 10) - [a, b]
+        b = randint(1, 10) - (a)
+        c = randint(1, 10) - (a) - (b)
         row1 = [
             [None, None, a],
             [None, c, None],
