f7b268fa0bf656d3595dc98a1765d78287e7d33f
mhosseinkaka <mhosseinkaka@gmail.com>
2025-02-09 21:54:39 +0330

    :hammer:

--- check.py
+++ check.py
@@ -1,8 +1,16 @@
 """
-
+1. check board is empty or no
 
 in this file, it is checked whether the game has come to an end or not.
 
 
 
 """
+def check_empty(row1):
+    for row in range(9):
+        for col in range(3):
+            if row1[row][col] == None:
+                return True
+    else:
+        return False       
+    
