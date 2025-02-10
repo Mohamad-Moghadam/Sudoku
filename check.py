e05f3ec41c544e97d2d6ca2b8ee8357c0ab5f9c3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-10 13:41:49 +0330

    🚧 adding the branch to my pc

--- check.py
+++ check.py
@@ -6,10 +6,12 @@ in this file, it is checked whether the game has come to an end or not.
 
 
 """
+
+
 def check_empty(row1):
     for row in range(9):
         for col in range(3):
             if row1[row][col] == None:
                 return True
     else:
-        return False 
+        return False
