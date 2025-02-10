cfa0a4dffcb763542289ad0b15371e733f4fe219
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-10 15:40:32 +0330

    🐛 fixing the counter with numbers

--- colours.py
+++ colours.py
@@ -11,11 +11,11 @@ in this file we change the colours of the numbers to be more user friendly.
 
 
 def printing(board):
-    for i in range(len(board)):
-        for j in range(len(board)):
+    for i in range(81):
+        for j in range(82):
             if j == 0:
-                print("┌")
-            elif j == 81:
+                print("┌", end="")
+            elif j == 80:
                 print("┐")
             else:
-                print("─")
+                print("─", end="")
