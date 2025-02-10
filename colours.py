11f5464c1fa1449318ee0fae2d814eb66a715e95
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-10 14:07:28 +0330

    ✨ adding the lines of the first row

--- colours.py
+++ colours.py
@@ -8,3 +8,15 @@ in this file we change the colours of the numbers to be more user friendly.
 
 
 """
+
+from menu import *
+
+
+def colours(board):
+    for i in range(len(board)):
+        if i == 0:
+            print("┌")
+        elif i == 81:
+            print("┐")
+        else:
+            print("─")
