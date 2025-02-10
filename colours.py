832168ef4fdb489ea46d6bd1d92bb65f5d23eb02
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-10 16:45:43 +0330

    🚧 adding unorganized charts

--- colours.py
+++ colours.py
@@ -12,10 +12,11 @@ in this file we change the colours of the numbers to be more user friendly.
 
 def printing(board):
     for i in range(81):
-        for j in range(82):
+        for j in range(81):
             if j == 0:
                 print("┌", end="")
             elif j == 80:
                 print("┐")
             else:
-                print("─", end="")
+                print(f"─│", end="")
+        print()
