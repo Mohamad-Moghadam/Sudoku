0b1e7acf310fa726dcec8f29f8d4adb0b2ef40b9
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-10 15:32:23 +0330

    ✨ adding another counter for j

--- colours.py
+++ colours.py
@@ -12,9 +12,10 @@ in this file we change the colours of the numbers to be more user friendly.
 
 def printing(board):
     for i in range(len(board)):
-        if i == 0:
-            print("┌")
-        elif i == 81:
-            print("┐")
-        else:
-            print("─")
+        for j in range(len(board)):
+            if j == 0:
+                print("┌")
+            elif j == 81:
+                print("┐")
+            else:
+                print("─")
