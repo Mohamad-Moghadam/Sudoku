fbc201fe86449b9be4e1bbaec22d13c7a3a87458
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-12 00:16:56 +0330

    🚀 added mhossain's code. printing in terminal is done

--- colours.py
+++ colours.py
@@ -8,36 +8,15 @@ in this file we change the colours of the numbers to be more user friendly.
 """
 
 
-def printing(board):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0:  # Print horizontal separator every 3 rows
-            print("-------------------------------------")
-        for j in range(len(board[i])):
-            cell = str(board[i][j]) if board[i][j] is not None else " "
-            end_char = " │ " if (j + 1) % 3 == 0 and j != 8 else " "
-            print(cell.center(3), end=end_char)
+def printing(Arr):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            if Arr[i][j] == None:
+                print(" ", end=" ")
+            else:
+                print(Arr[i][j], end=" ")
         print()
-
-
-"""if board[i] == None:
-            board[i] = " "
-        if i % 9 == 8:
-            print(f"{board[i]}")
-        elif (
-            i % 3 == 2
-            and i != 8
-            and i != 17
-            and i != 26
-            and i != 35
-            and i != 44
-            and i != 53
-            and i != 62
-            and i != 71
-            and i != 80
-        ):
-            print(f"{board[i]}│", end="")
-        else:
-            print(f"{board[i]}", end="")
-        if i == 26 or i == 53:
-            print("___________")
-"""
