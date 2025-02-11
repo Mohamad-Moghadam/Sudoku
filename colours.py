28a71f1073079798f16ab16ffcc76f718550dfc6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-11 21:59:38 +0330

    🔨 changing the style of lists in terminal

--- colours.py
+++ colours.py
@@ -7,10 +7,35 @@ in this file we change the colours of the numbers to be more user friendly.
 
 """
 
+board = [
+    [3, 0, 6, 5, 0, 8, 4, 0, 0],
+    [5, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 7, 0, 0, 0, 0, 3, 1],
+    [0, 0, 3, 0, 1, 0, 0, 8, 0],
+    [9, 0, 0, 8, 6, 3, 0, 0, 5],
+    [0, 5, 0, 0, 9, 0, 6, 0, 0],
+    [1, 3, 0, 0, 0, 0, 2, 5, 0],
+    [0, 0, 0, 0, 0, 0, 9, 7, 4],
+    [0, 0, 5, 2, 0, 6, 3, 0, 9],
+]
+
 
 def printing(board):
     for i in range(len(board)):
-        if board[i] == None:
+        for j in range(len(board[i])):
+            if board[i][j] == None:
+                board[i][j] = " "
+            elif j % 3 == 2:
+                print(f"{board[i][j]}│", end="")
+            else:
+                print(f"{board[i][j]}", end="")
+        print("___________")
+
+
+printing(board)
+
+
+"""if board[i] == None:
             board[i] = " "
         if i % 9 == 8:
             print(f"{board[i]}")
@@ -31,3 +56,4 @@ def printing(board):
             print(f"{board[i]}", end="")
         if i == 26 or i == 53:
             print("___________")
+"""
