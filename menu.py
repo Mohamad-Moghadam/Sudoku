538e011992c9093af3501f817710689f81c5e586
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-02-11 14:32:59 +0330

    ⚙️ adding json for statistics

--- menu.py
+++ menu.py
@@ -44,7 +44,7 @@ def menu():
                     "r",
                     encoding="utf-8",
                 ) as file:
-                    data = json.load(file)
+                    data = json.dumps(file)
                 print(data)
                 return
         elif choice == 3:
