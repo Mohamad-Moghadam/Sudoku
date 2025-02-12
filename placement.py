ab480a360c9d1a8569cd2abf84db929b0546acdb
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-12 19:26:53 +0330

    ✨ trying to change the colour of each number

--- placement.py
+++ placement.py
@@ -4,9 +4,21 @@ in this file, we decide where the player have set their numbers and whether it i
 
 """
 
+from check import *
+
 
 def update(board, inserted):
     row = inserted[0]
     column = inserted[1]
     board[row][column] = inserted[2]
+    if check_number_row_col(board) and check_3x3_square(board):
+        inserted[2] = f"\033[92m{inserted[2]}\033[0m"
     return board
+
+    """
+    Here we place the inserted numbers.
+    
+    ---------------------------
+    returns:
+        updated board
+    """
